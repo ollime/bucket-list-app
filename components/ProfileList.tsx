d3068e8293d4dfe9e234b04aab5da6cab1485dea
Jennie Long <longjen100@gmail.com>
2025-07-04 15:29:33 -0700

    added avatar image next to list item

--- components/ProfileList.tsx
+++ components/ProfileList.tsx
@@ -1,8 +1,9 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { FlashList } from '@shopify/flash-list';
 
 interface ListItemData {
   title: string;
+  avatarUrl?: string;
 }
 
 interface ProfileListProps {
@@ -15,7 +16,14 @@ export default function ProfileList({ data }: ProfileListProps) {
       data={data}
       renderItem={({ item }) => (
         <View className={styles.itemContainer}>
-          <Text>Test</Text>
+          {item.avatarUrl ? (
+            <Image
+              source={{ uri: item.avatarUrl }}
+              accessibilityLabel="Avatar"
+              className="size-[40px] overflow-hidden rounded-full object-cover pt-0"></Image>
+          ) : (
+            <View className="size-[40px] rounded-full border-2 border-black bg-white"></View>
+          )}
           <Text className={styles.item}>{item.title}</Text>
         </View>
       )}
@@ -26,6 +34,6 @@ export default function ProfileList({ data }: ProfileListProps) {
 }
 
 const styles = {
-  itemContainer: 'flex flex-row',
-  item: 'p-2',
+  itemContainer: 'flex flex-row items-center',
+  item: 'p-2 m-2',
 };
