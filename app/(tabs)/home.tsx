92238a04dcaa26709a99501252afcefc0ec69707
Jennie Long <longjen100@gmail.com>
2025-07-05 00:35:00 -0700

    profile list downloads & renders image

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import { Container } from 'components/Container';
-import ProfileList from 'components/ProfileList';
+import ProfileList, { ListItemData } from 'components/ProfileList';
+import { supabase } from 'utils/supabase';
 
 export default function Home() {
-  const DATA = [{ title: 'First item' }, { title: 'Second item' }];
+  const [data, setData] = useState<ListItemData[]>([]);
+
+  async function getAllUsers() {
+    const { data: users, error } = await supabase.from('profiles').select('*');
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    console.log(users);
+
+    const formattedData: ListItemData[] = [];
+    users.forEach(({ id, username, avatar_url }) => {
+      formattedData.push({ title: username, avatarUrl: avatar_url });
+    });
+    setData(formattedData);
+  }
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
   return (
     <>
       <Container>
         <Text>Hello</Text>
-        <ProfileList data={DATA}></ProfileList>
+        <ProfileList data={data}></ProfileList>
       </Container>
     </>
   );
--- components/ProfileList.tsx
+++ components/ProfileList.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import { Text, View, Image } from 'react-native';
 import { FlashList } from '@shopify/flash-list';
+import { supabase } from 'utils/supabase';
 
-interface ListItemData {
+export interface ListItemData {
   title: string;
   avatarUrl?: string;
 }
@@ -11,22 +13,56 @@ interface ProfileListProps {
 }
 
 export default function ProfileList({ data }: ProfileListProps) {
+  function ProfileListItem({ item }: { item: ListItemData }) {
+    const [avatarUri, setAvatarUri] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+      let isMounted = true;
+      async function downloadImage(path: string) {
+        try {
+          const { data, error } = await supabase.storage.from('avatars').download(path);
+          if (error) {
+            throw error;
+          }
+          const fr = new FileReader();
+          fr.readAsDataURL(data);
+          fr.onload = () => {
+            if (isMounted) setAvatarUri(fr.result as string);
+          };
+        } catch (error) {
+          if (error instanceof Error) {
+            alert('Error downloading image: ' + error.message);
+          }
+        }
+      }
+      if (item.avatarUrl) {
+        downloadImage(item.avatarUrl);
+      }
+      return () => {
+        isMounted = false;
+      };
+    }, [item.avatarUrl]);
+
+    return (
+      <View className={styles.itemContainer}>
+        {item.avatarUrl && avatarUri ? (
+          <Image
+            source={{ uri: avatarUri }}
+            accessibilityLabel="Avatar"
+            className="size-[40px] overflow-hidden rounded-full object-cover pt-0"
+          />
+        ) : (
+          <View className="size-[40px] rounded-full border-2 border-black bg-white"></View>
+        )}
+        <Text className={styles.item}>{item.title}</Text>
+      </View>
+    );
+  }
+
   return (
     <FlashList
       data={data}
-      renderItem={({ item }) => (
-        <View className={styles.itemContainer}>
-          {item.avatarUrl ? (
-            <Image
-              source={{ uri: item.avatarUrl }}
-              accessibilityLabel="Avatar"
-              className="size-[40px] overflow-hidden rounded-full object-cover pt-0"></Image>
-          ) : (
-            <View className="size-[40px] rounded-full border-2 border-black bg-white"></View>
-          )}
-          <Text className={styles.item}>{item.title}</Text>
-        </View>
-      )}
+      renderItem={({ item }) => <ProfileListItem item={item} />}
       contentContainerStyle={{ backgroundColor: 'white', padding: 2 }}
       estimatedItemSize={48}
     />
