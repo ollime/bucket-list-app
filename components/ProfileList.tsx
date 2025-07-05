63fa3a5907da8b2ff29669bf262a1cc828a28aff
Jennie Long <longjen100@gmail.com>
2025-07-05 00:41:15 -0700

    added margin to profile list items

--- components/ProfileList.tsx
+++ components/ProfileList.tsx
@@ -13,52 +13,6 @@ interface ProfileListProps {
 }
 
 export default function ProfileList({ data }: ProfileListProps) {
-  function ProfileListItem({ item }: { item: ListItemData }) {
-    const [avatarUri, setAvatarUri] = useState<string | undefined>(undefined);
-
-    useEffect(() => {
-      let isMounted = true;
-      async function downloadImage(path: string) {
-        try {
-          const { data, error } = await supabase.storage.from('avatars').download(path);
-          if (error) {
-            throw error;
-          }
-          const fr = new FileReader();
-          fr.readAsDataURL(data);
-          fr.onload = () => {
-            if (isMounted) setAvatarUri(fr.result as string);
-          };
-        } catch (error) {
-          if (error instanceof Error) {
-            alert('Error downloading image: ' + error.message);
-          }
-        }
-      }
-      if (item.avatarUrl) {
-        downloadImage(item.avatarUrl);
-      }
-      return () => {
-        isMounted = false;
-      };
-    }, [item.avatarUrl]);
-
-    return (
-      <View className={styles.itemContainer}>
-        {item.avatarUrl && avatarUri ? (
-          <Image
-            source={{ uri: avatarUri }}
-            accessibilityLabel="Avatar"
-            className="size-[40px] overflow-hidden rounded-full object-cover pt-0"
-          />
-        ) : (
-          <View className="size-[40px] rounded-full border-2 border-black bg-white"></View>
-        )}
-        <Text className={styles.item}>{item.title}</Text>
-      </View>
-    );
-  }
-
   return (
     <FlashList
       data={data}
@@ -69,7 +23,54 @@ export default function ProfileList({ data }: ProfileListProps) {
   );
 }
 
+function ProfileListItem({ item }: { item: ListItemData }) {
+  const [avatarUri, setAvatarUri] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    let isMounted = true;
+    async function downloadImage(path: string) {
+      try {
+        const { data, error } = await supabase.storage.from('avatars').download(path);
+        if (error) {
+          throw error;
+        }
+        const fr = new FileReader();
+        fr.readAsDataURL(data);
+        fr.onload = () => {
+          if (isMounted) setAvatarUri(fr.result as string);
+        };
+      } catch (error) {
+        if (error instanceof Error) {
+          alert('Error downloading image: ' + error.message);
+        }
+      }
+    }
+    if (item.avatarUrl) {
+      downloadImage(item.avatarUrl);
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, [item.avatarUrl]);
+
+  return (
+    <View className={styles.itemContainer}>
+      {item.avatarUrl && avatarUri ? (
+        <Image
+          source={{ uri: avatarUri }}
+          accessibilityLabel="Avatar"
+          className={styles.itemImage}
+        />
+      ) : (
+        <View className="size-[40px] rounded-full border-2 border-black bg-white"></View>
+      )}
+      <Text className={styles.itemLabel}>{item.title}</Text>
+    </View>
+  );
+}
+
 const styles = {
   itemContainer: 'flex flex-row items-center',
-  item: 'p-2 m-2',
+  itemLabel: 'p-2 m-2',
+  itemImage: 'm-2 size-[40px] overflow-hidden rounded-full object-cover pt-0',
 };
