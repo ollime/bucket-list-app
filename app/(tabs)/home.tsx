5c57f20cbe69cbc747975c70eabf54d92f294338
Jennie Long <longjen100@gmail.com>
2025-07-05 01:36:57 -0700

    moved profile list to search tab

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,37 +1,12 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import { Text } from 'react-native';
 import { Container } from 'components/Container';
-import ProfileList, { ListItemData } from 'components/ProfileList';
-import { supabase } from 'utils/supabase';
 
 export default function Home() {
-  const [data, setData] = useState<ListItemData[]>([]);
-
-  async function getAllUsers() {
-    const { data: users, error } = await supabase.from('profiles').select('*');
-    if (error) {
-      alert(error.message);
-      return;
-    }
-
-    console.log(users);
-
-    const formattedData: ListItemData[] = [];
-    users.forEach(({ id, username, avatar_url }) => {
-      formattedData.push({ title: username, avatarUrl: avatar_url });
-    });
-    setData(formattedData);
-  }
-
-  useEffect(() => {
-    getAllUsers();
-  }, []);
-
   return (
     <>
       <Container>
         <Text>Hello</Text>
-        <ProfileList data={data}></ProfileList>
       </Container>
     </>
   );
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import { Container } from 'components/Container';
+import ProfileList, { ListItemData } from 'components/ProfileList';
+import { supabase } from 'utils/supabase';
 
-export default function Home() {
+export default function Search() {
+  const [data, setData] = useState<ListItemData[]>([]);
+
+  async function getAllUsers() {
+    const { data: users, error } = await supabase.from('profiles').select('*');
+    if (error) {
+      alert(error.message);
+      return;
+    }
+    const formattedData: ListItemData[] = [];
+    users.forEach(({ id, username, avatar_url }) => {
+      formattedData.push({ title: username, avatarUrl: avatar_url });
+    });
+    setData(formattedData);
+  }
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
   return (
     <>
       <Container>
         <Text>Hello</Text>
+        <ProfileList data={data}></ProfileList>
       </Container>
     </>
   );
