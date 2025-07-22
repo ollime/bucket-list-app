c263b288be3ec1d6124f961a3fa1e4db8422d115
Jennie Long <longjen100@gmail.com>
2025-07-22 01:58:37 -0700

    determines friend status on profile list

--- app/(tabs)/friends/index.tsx
+++ app/(tabs)/friends/index.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 import { Container } from 'components/Container';
 import ProfileList from 'components/ProfileList';
 import { useSession } from 'utils/context';
-import { getFriends } from 'utils/api';
-import { ProfileData } from 'utils/Profile.types';
+import { getUsername, getFriends } from 'utils/api';
+import { ProfileData, ProfileWithoutFriend } from 'utils/Profile.types';
 
 export default function Friends() {
   const [data, setData] = useState<ProfileData[]>([]);
@@ -13,23 +13,27 @@ export default function Friends() {
     async function getData() {
       const data = await getFriends(session ?? undefined);
       const formattedData: ProfileData[] = [];
+      const username = await getUsername(session ?? undefined);
       if (data) {
-        // for (let friend of data) {
-        //   if ('username' in friend.friend && 'avatar_url' in friend.friend) {
-        //     const status = friend.status;
-        //     const { username, avatar_url } = friend.friend as {
-        //       username: string;
-        //       avatar_url: string;
-        //     };
-        //     formattedData.push({
-        //       username: username,
-        //       avatarUrl: avatar_url,
-        //       friendStatus: status,
-        //     });
-        //   }
-        // }
-
-        console.log(data);
+        for (let friend of data) {
+          const sender = friend.sender as unknown as ProfileWithoutFriend;
+          const receiver = friend.receiver as unknown as ProfileWithoutFriend;
+          const displayedUser = sender?.username === username ? receiver : sender;
+          const isSender = sender?.username === username;
+          if ('username' in displayedUser && 'avatar_url' in displayedUser) {
+            const status =
+              friend.status === 'pending' ? (isSender ? 'requested' : 'pending') : 'none';
+            const { username, avatar_url } = displayedUser as {
+              username: string;
+              avatar_url: string;
+            };
+            formattedData.push({
+              username: username,
+              avatarUrl: avatar_url,
+              friendStatus: status,
+            });
+          }
+        }
       }
       return formattedData;
     }
--- utils/Profile.types.ts
+++ utils/Profile.types.ts
@@ -2,4 +2,12 @@ export interface ProfileData {
   username: string;
   avatarUrl?: string;
   friendStatus: 'none' | 'requested' | 'pending' | 'accepted';
+  // 'requested' indicates that the user sent a request and is waiting for a response
+  // 'pending' indicates that the user has received the request
+  // both of these states are recorded as 'pending' in the database but is stored differently here for different ui states
+}
+
+export interface ProfileWithoutFriend {
+  username: string;
+  avatarUrl?: string;
 }
