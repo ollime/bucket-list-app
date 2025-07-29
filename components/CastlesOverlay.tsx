f433040964ff81a5cfabd27a7b9cbabec43b7fce
Jennie Long <longjen100@gmail.com>
2025-07-29 15:26:29 -0700

    modified castle grid logic

--- components/CastlesOverlay.tsx
+++ components/CastlesOverlay.tsx
@@ -3,20 +3,26 @@ import { Image } from 'expo-image';
 
 export default function CastlesOverlay() {
   const screenWidth = Dimensions.get('window').width;
-  const maxRows = 3;
   const imagesPerRow = Math.floor(screenWidth / 50);
 
   const numOfCastles = 15;
-  const maxNumOfCastles = imagesPerRow * maxRows;
+  const maxNumOfCastles = imagesPerRow;
 
   let images = [];
+
+  let topVariants = [0, 2, 3];
+  let bottomVariants = [2, 1, 1];
 
   for (let i = 0; i < numOfCastles; i++) {
     if (i < maxNumOfCastles) {
       images.push(
         <View>
-          <CastleTrim variant={i}></CastleTrim>
-          <CastleImage variant={i}></CastleImage>
+          <>
+            {/* top castle */}
+            <CastleImage variant={topVariants[i % 3]}></CastleImage>
+            {/* bottom castle */}
+            <CastleImage variant={bottomVariants[i % 3]}></CastleImage>
+          </>
         </View>
       );
     }
@@ -24,59 +30,89 @@ export default function CastlesOverlay() {
 
   return (
     <View className="absolute bottom-[0px] left-[-1px] z-[2] flex w-full flex-1">
-      <View className={`flex flex-row flex-wrap-reverse justify-center`}>{images}</View>
+      <View className={`flex flex-row flex-wrap-reverse items-end justify-center`}>{images}</View>
     </View>
   );
 }
 
-function CastleTrim({ variant }: { variant: number }) {
-  return (
-    <>
-      {variant === 1 ? (
-        <Image
-          source={require('assets/castles/empty-trim.png')}
-          style={{
-            width: 50,
-            height: 10,
-            aspectRatio: 1,
-          }}
-          contentFit="cover"></Image>
-      ) : (
-        <Image
-          source={require('assets/castles/castle-trim.png')}
-          style={{
-            width: 50,
-            height: 10,
-            aspectRatio: 1,
-          }}
-          contentFit="cover"></Image>
-      )}
-    </>
-  );
+/**
+ *
+ * @param variant variant style
+ *    1: empty
+ *    2: filled
+ *    3: castle
+ * @returns
+ */
+function CastleImage({ variant }: { variant: number }) {
+  function getImage() {
+    switch (variant) {
+      case 0:
+        return (
+          <>
+            <Image
+              source={require('assets/castles/empty-trim.png')}
+              style={styles.trim}
+              contentFit="cover"></Image>
+            <Image
+              source={require('assets/castles/empty.png')}
+              style={styles.castle}
+              contentFit="cover"></Image>
+          </>
+        );
+      case 1:
+        return (
+          <>
+            <Image
+              source={require('assets/castles/castle-full.png')}
+              style={styles.trim}
+              contentFit="cover"></Image>
+            <Image
+              source={require('assets/castles/castle.png')}
+              style={styles.castle}
+              contentFit="cover"></Image>
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <Image
+              source={require('assets/castles/castle-trim.png')}
+              style={styles.trim}
+              contentFit="cover"></Image>
+            <Image
+              source={require('assets/castles/castle.png')}
+              style={styles.castle}
+              contentFit="cover"></Image>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <Image
+              source={require('assets/castles/castle-top-trim.png')}
+              style={styles.trim}
+              contentFit="cover"></Image>
+            <Image
+              source={require('assets/castles/castle-top.png')}
+              style={styles.castle}
+              contentFit="cover"></Image>
+          </>
+        );
+    }
+  }
+
+  return <>{getImage()}</>;
 }
 
-function CastleImage({ variant }: { variant: number }) {
-  return (
-    <>
-      {variant === 1 ? (
-        <Image
-          source={require('assets/castles/empty.png')}
-          style={{
-            width: 50,
-            height: 40,
-            aspectRatio: 1,
-          }}
-          contentFit="cover"></Image>
-      ) : (
-        <Image
-          source={require('assets/castles/castle.png')}
-          style={{
-            width: 50,
-            height: 40,
-            aspectRatio: 1,
-          }}
-          contentFit="cover"></Image>
-      )}
-    </>
-  );
-}
+const styles = {
+  castle: {
+    width: 50,
+    height: 40,
+    aspectRatio: 1,
+  },
+  trim: {
+    width: 50,
+    height: 10,
+    aspectRatio: 1,
+  },
+};
